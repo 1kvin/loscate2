cee1dcfe639498d67b22f17c1257f511de3e079c
artem.ustinov <artem.ustinov@corp.mail.ru>
2021-11-22 23:54:02 +0300

    Added API for working with the map

---
+++ Loscate.Site/Controllers/Map/GetPinsController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Loscate.Site.DbContext;
+using Loscate.Site.Utilities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pin = Loscate.DTO.Map.Pin;
+
+namespace Loscate.Site.Controllers.Map
+{
+    [Authorize]
+    [Route("api/map/[controller]")]
+    public class GetPinsController: Controller
+    {
+        private readonly LoscateDbContext loscateDbContext;
+
+        public GetPinsController(LoscateDbContext loscateDbContext)
+        {
+            this.loscateDbContext = loscateDbContext;
+        }
+
+        [HttpGet]
+        public List<Pin> Get()
+        {
+            return loscateDbContext.Pins.Include(p=>p.User).ToList().Select(pin => pin.ConvertToDto()).ToList();
+        }
+    }
+}
---
+++ Loscate.Site/Controllers/Map/SearchPinController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Loscate.Site.DbContext;
+using Loscate.Site.Utilities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pin = Loscate.DTO.Map.Pin;
+
+namespace Loscate.Site.Controllers.Map
+{
+    [Authorize]
+    [Route("api/map/[controller]")]
+    public class SearchPinController : Controller
+    {
+        private readonly LoscateDbContext loscateDbContext;
+
+        public SearchPinController(LoscateDbContext loscateDbContext)
+        {
+            this.loscateDbContext = loscateDbContext;
+        }
+
+        [HttpGet]
+        public List<Pin> Get(string filter)
+        {
+            if (string.IsNullOrEmpty(filter))
+            {
+                return loscateDbContext.Pins.Include(p => p.User).ToList().Select(pin => pin.ConvertToDto()).ToList();
+            }
+            else
+            {
+                return loscateDbContext.Pins
+                    .Include(p => p.User)
+                    .ToList()
+                    .Select(pin => pin.ConvertToDto())
+                    .Where(pin => (pin.ShortName.Contains(filter) || pin.FullName.Contains(filter)))
+                    .ToList();
+            }
+        }
+    }
+}
