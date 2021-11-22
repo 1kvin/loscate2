069fb353e8ec4fec3a3e9b85205ecd84887f3f1e
1kvin <kvashnin.aa@edu.spbstu.ru>
2021-11-22 22:46:30 +0300

    add Site project template

---
+++ Loscate.Site/DbContext/FirebaseUser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Loscate.Site.DbContext
+{
+    public partial class FirebaseUser
+    {
+        public FirebaseUser()
+        {
+            ChatMessages = new HashSet<ChatMessage>();
+            DialogUserId1Navigations = new HashSet<Dialog>();
+            DialogUserId2Navigations = new HashSet<Dialog>();
+            Pins = new HashSet<Pin>();
+        }
+
+        public int Id { get; set; }
+        public string Uid { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PictureUrl { get; set; }
+        public string PhotoBase64 { get; set; }
+
+        public virtual ICollection<ChatMessage> ChatMessages { get; set; }
+        public virtual ICollection<Dialog> DialogUserId1Navigations { get; set; }
+        public virtual ICollection<Dialog> DialogUserId2Navigations { get; set; }
+        public virtual ICollection<Pin> Pins { get; set; }
+    }
+}
