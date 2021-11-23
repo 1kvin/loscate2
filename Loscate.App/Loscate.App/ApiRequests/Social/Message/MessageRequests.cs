566bf63c81274c416ec76834ae273c35510d41e0
bogdan.mashchenko <bogdan.mashchenko@corp.mail.ru>
2021-11-23 14:21:38 +0300

    API for chat and dialogs

---
+++ Loscate.App/Loscate.App/ApiRequests/Social/Message/MessageRequests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Loscate.App.Services;
+using Loscate.App.Utilities;
+
+namespace Loscate.App.ApiRequests.Social.Message
+{
+    public static class MessageRequests
+    {
+        public static async Task<List<DTO.Social.Message>> GetUserMessage(IFirebaseAuthenticator firebaseAuthenticator,
+            string pairUid)
+        {
+            return await ApiRequest.MakeRequest<List<DTO.Social.Message>>(firebaseAuthenticator,
+                $"api/social/message/getDialogMessages?pairUid={pairUid}");
+        }
+
+        public static async Task<string> SendMessage(IFirebaseAuthenticator firebaseAuthenticator, string pairUid,
+            string msg)
+        {
+            return await ApiRequest.MakeRequest<string>(firebaseAuthenticator,
+                $"api/social/message/sendMessage?pairUid={pairUid}&msg={msg}");
+        }
+    }
+}
