8e0c13602098c9979a08f6be47f76f1133efbf6e
lindedaniil <linde3315@gmail.com>
2021-11-23 15:36:22 +0300

    Implement Page Set API My Profile

---
+++ Loscate.App/Loscate.App/Models/XamarinDialog.cs
@@ -0,0 +1,16 @@
+﻿using Loscate.DTO.Social;
+
+namespace Loscate.App.Models
+{
+    public class XamarinDialog : Dialog
+    {
+        public XamarinUser XamarinCompanion { get; set; }
+
+        public XamarinDialog(Dialog dialog)
+        {
+            Companion = dialog.Companion;
+            XamarinCompanion = new XamarinUser(Companion);
+            this.LastMessage = dialog.LastMessage;
+        }
+    }
+}
