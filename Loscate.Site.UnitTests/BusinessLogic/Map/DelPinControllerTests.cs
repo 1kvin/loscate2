b9af3cb3da1d82cd003d372de13a1af27d4f34f2
lindedaniil <linde3315@gmail.com>
2022-02-24 17:52:43 +0300

    Add test for deleting other users pin

--- Loscate.Site.UnitTests/BusinessLogic/Map/DelPinControllerTests.cs
+++ Loscate.Site.UnitTests/BusinessLogic/Map/DelPinControllerTests.cs
@@ -125,5 +125,60 @@ namespace Loscate.Site.Controllers.Map.Tests
             Assert.AreNotEqual(result, "OK");
         }
 
+        [TestMethod]
+        public void DelByExistsFromOtherUserTest()
+
+        {
+            const int testUserId2 = 1;
+            const string testUserUid2 = "Uid2";
+            var testUser1 = new FirebaseUser { Id = testUserId, Uid = testUserUid };
+            var testUser2 = new FirebaseUser { Id = testUserId2, Uid = testUserUid2 };
+
+            var users = new List<FirebaseUser>()
+            {
+                testUser1,
+                testUser2
+            }.AsQueryable();
+
+            var pins = new List<Pin>()
+            {
+                new()
+                {
+                    Latitude = testPinLatitude,
+                    Longitude = testPinLongitude,
+                    Photo = testPinPhoto,
+                    FullName = testPinFullName,
+                    ShortName = testPinShortName,
+                    User = testUser1,
+                    UserId = testUser1.Id
+                }
+            }.AsQueryable();
+
+            var pinsMockSet = new Mock<DbSet<Pin>>();
+            pinsMockSet.As<IQueryable<Pin>>().Setup(m => m.Provider).Returns(pins.Provider);
+            pinsMockSet.As<IQueryable<Pin>>().Setup(m => m.Expression).Returns(pins.Expression);
+            pinsMockSet.As<IQueryable<Pin>>().Setup(m => m.ElementType).Returns(pins.ElementType);
+            pinsMockSet.As<IQueryable<Pin>>().Setup(m => m.GetEnumerator()).Returns(pins.GetEnumerator());
+
+            var userMockSet = new Mock<DbSet<FirebaseUser>>();
+            userMockSet.As<IQueryable<FirebaseUser>>().Setup(m => m.Provider).Returns(users.Provider);
+            userMockSet.As<IQueryable<FirebaseUser>>().Setup(m => m.Expression).Returns(users.Expression);
+            userMockSet.As<IQueryable<FirebaseUser>>().Setup(m => m.ElementType).Returns(users.ElementType);
+            userMockSet.As<IQueryable<FirebaseUser>>().Setup(m => m.GetEnumerator()).Returns(users.GetEnumerator());
+
+            var mockContextDb = new Mock<ILoscateDbRepository>();
+            mockContextDb.Setup(c => c.Pins).Returns(pinsMockSet.Object);
+            mockContextDb.Setup(c => c.FirebaseUsers).Returns(userMockSet.Object);
+
+            var mockUserService = new Mock<IUserService>();
+            mockUserService.Setup(c => c.GetDbUser(It.IsAny<ClaimsPrincipal>())).Returns(testUser2);
+
+            var controller = new DelPinController(mockContextDb.Object, mockUserService.Object);
+            var result = controller.Get(1);
+
+            Assert.IsNotNull(result);
+            Assert.AreNotEqual(result, "OK");
+        }
+
     }
 }
